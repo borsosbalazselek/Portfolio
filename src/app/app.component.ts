e7233acedc0765909a41c5015c5043d28ccc70dc
borsosbalazselek <borsosbalazs1979@gmail.com>
2025-08-07 23:04:01 +0200

    AOS optimalization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, Renderer2  } from '@angular/core';
+import { Component, OnInit, Renderer2, AfterViewInit  } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
-import AOS from 'aos';
 
 @Component({
     selector: 'app-root',
@@ -9,19 +8,34 @@ import AOS from 'aos';
     styleUrl: './app.component.css',
     standalone: false
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements  AfterViewInit {
   title = 'myportfolio';
 
   constructor(private translate: TranslateService, private renderer: Renderer2) {
     translate.setDefaultLang('hu');
   }
-  ngOnInit() {
-    AOS.init({
+  async ngAfterViewInit() {
+    // CSS
+    const link = this.renderer.createElement('link');
+    this.renderer.setAttribute(link, 'rel', 'stylesheet');
+    this.renderer.setAttribute(link, 'href', 'https://unpkg.com/aos@2.3.4/dist/aos.css');
+    this.renderer.appendChild(document.head, link);
+
+    // JS
+    await new Promise<void>((resolve, reject) => {
+      const script = this.renderer.createElement('script');
+      script.src = 'https://unpkg.com/aos@2.3.4/dist/aos.js';
+      script.onload = () => resolve();
+      script.onerror = () => reject();
+      this.renderer.appendChild(document.body, script);
+    });
+
+    // Globális AOS elérése
+    (window as any).AOS.init({
       duration: 800,
       easing: 'ease-in-out',
       once: true
     });
-    
   }
   
 }
